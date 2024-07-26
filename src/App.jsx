4bcb73387e78539ae4e8aeb6c6fbd5e740be3659
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-07-26 14:27:22 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,17 @@
 import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import "./App.css";
 import HomePage from "./Pages/HomePage";
+import Login from "./Pages/Login";
 
 function App() {
   const routes = createBrowserRouter([
     {
       path: "/",
       element: <HomePage />,
+    },
+    {
+      path: "/login",
+      element: <Login />,
     },
   ]);
   return (
---
+++ src/Pages/Login.jsx
@@ -0,0 +1,65 @@
+// import React from 'react'
+import { Link } from "react-router-dom";
+import styles from "../styles/Login.module.css";
+
+const Login = () => {
+  return (
+    <div className={styles.wrapper}>
+      <div className={styles.login_info}>
+        <img src="/svg/Logo.svg" alt="" />
+        <div className={styles.login_header}>
+          <h1>Welcome Back!</h1>
+          <p>
+            Log In to your account using any of these method to explore more
+            collection.
+          </p>
+          <div className={styles.login_socials}>
+            <button>
+              <img src="/svg/google.svg" alt="" />
+              Google
+            </button>
+            <button>
+              <img src="/svg/facebook_1.svg" alt="" />
+              Facebook
+            </button>
+          </div>
+        </div>
+        <div className={styles.form_area}>
+          <div className={styles.form_header}>
+            <span className={styles.line}></span>
+            <p>Or continue with email</p>
+            <span className={styles.line}></span>
+          </div>
+          <form action="" className={styles.login_details}>
+            <div className={styles.form_fill}>
+              <div className={styles.form_input}>
+                <img src="/svg/mailIcon.svg" alt="" />
+                <input type="text" placeholder="Email Address" />
+              </div>
+              <div className={styles.form_input}>
+                <img src="/svg/lock.svg" alt="" />
+                <input type="password" placeholder="Password" />
+                <img src="/svg/pass_hide.svg" alt="" />
+              </div>
+              <span className={styles.rem_pass}>
+                <div>
+                  <input type="checkbox" name="" id="" />
+                  <p>Remember me</p>
+                </div>
+                <p>Forget Password?</p>
+              </span>
+            </div>
+            <button>Login</button>
+            <footer>
+              <h1>Don&rsquo;t have account?</h1>
+              <Link>Sign Up</Link>
+            </footer>
+          </form>
+        </div>
+      </div>
+      <div className={styles.carve}></div>
+    </div>
+  );
+};
+
+export default Login;
