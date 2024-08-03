4f7e799b65278cf455304ea7d549886e2a8b7cfe
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-08-03 15:08:09 +0100

    updates

---
+++ Backend/server.js
@@ -0,0 +1,9 @@
+// import express from "express";
+
+// const app = express();
+
+// app.get("/api/products", (req, res) => {
+//   res.json(products);
+// });
+
+// app.listen(8000, () => console.log("server is running on port 8000"));
