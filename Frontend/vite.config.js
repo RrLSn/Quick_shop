ea8866fcc02ee27017b79b87f80391a1d6699bdc
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-09-15 12:30:53 +0100

    updates

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -4,4 +4,9 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Default port for Vite, but you can change it if needed
+    open: true, // This will automatically open the browser when the server starts
+    historyApiFallback: true, // This ensures that all routes fall back to index.html
+  },
 });
