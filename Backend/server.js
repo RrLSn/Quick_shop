31307934285739dd59ac440f00880bdcc70d768a
RrLSn <afolabisodiqtemitope@yahoo.com>
2024-10-30 10:37:54 +0100

    CORS update

--- Backend/server.js
+++ Backend/server.js
@@ -34,7 +34,7 @@ const mongoUri = process.env.MONGO_URI;
 // Enable CORS for all routes and origins
 app.use(
   cors({
-    origin: ["http://localhost:5173", "https://quick-shop-beryl.vercel.app/"],
+    origin: ["http://localhost:5173", "https://quick-shop-beryl.vercel.app"],
     credentials: true,
     methods: ["GET", "PUT", "POST", "DELETE"],
   })
